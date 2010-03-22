b5db1be9550bbca8040252efbd1a2329549bd53c
jzaefferer <joern.zaefferer@gmail.com>
2010-03-22 04:03:16 +0100

    Updated placeholder

---
+++ ui/jquery.ui.panel.js
@@ -0,0 +1,20 @@
+/*
+ * jQuery UI Panel @VERSION
+ *
+ * Copyright (c) 2010 AUTHORS.txt (http://jqueryui.com/about)
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * http://docs.jquery.com/UI/Panel
+ *
+ * Depends:
+ *	jquery.ui.core.js
+ *	jquery.ui.widget.js
+ */
+(function($) {
+
+$.widget("ui.panel", {
+
+});
+
+});
