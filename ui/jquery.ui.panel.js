df534ee7524cbbec11aa9a64e7a78e1184850964
jzaefferer <joern.zaefferer@gmail.com>
2010-03-22 16:28:20 +0100

    Adding visual test for panel and a base implementation; duplicated accordion.css content with panel-classes, to be merged later

--- ui/jquery.ui.panel.js
+++ ui/jquery.ui.panel.js
@@ -14,7 +14,29 @@
 (function($) {
 
 $.widget("ui.panel", {
+	_create: function() {
+		this.element.addClass("ui-panel ui-widget ui-helper-reset");
+		
+		var self = this;
+		this.headers = this.element.find("> li > :first-child,> :not(li):even").addClass("ui-panel-header ui-helper-reset ui-state-default ui-corner-all")
+			.bind("mouseenter.panel", function() { $(this).addClass('ui-state-hover'); })
+			.bind("mouseleave.panel", function() { $(this).removeClass('ui-state-hover'); })
+			.bind("focus.panel", function() { $(this).addClass('ui-state-focus'); })
+			.bind("blur.panel", function() { $(this).removeClass('ui-state-focus'); })
+			.bind("click.panel", function(e) { self.click($(this), e); return false; });
 
+		this.headers
+			.next()
+				.addClass("ui-panel-content ui-helper-reset ui-widget-content ui-corner-bottom")
+				.hide();
+	},
+	destroy: function() {
+		$.widget.prototype.destroy.call(this);
+	},
+	click: function(header, event) {
+		header.toggleClass("ui-state-active ui-corner-top ui-corner-all");
+		header.next().toggleClass("ui-panel-content-active").slideToggle("fast");
+	}
 });
 
-});
+}(jQuery));
