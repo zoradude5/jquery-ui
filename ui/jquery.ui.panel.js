80969865d7803ebfe1a473e2334be1aac5e5af72
jzaefferer <joern.zaefferer@gmail.com>
2010-03-22 17:06:47 +0100

    Panel: Code formatting

--- ui/jquery.ui.panel.js
+++ ui/jquery.ui.panel.js
@@ -26,16 +26,24 @@ $.widget("ui.panel", {
 		this.element.addClass("ui-panel ui-widget ui-helper-reset");
 		
 		var self = this;
-		this.headers = this.element.find("> li > :first-child,> :not(li):even").addClass("ui-panel-header ui-helper-reset ui-state-default ui-corner-all")
+		this.headers = this.element
+			.find("> li > :first-child,> :not(li):even")
+			.addClass("ui-panel-header ui-helper-reset ui-state-default ui-corner-all")
 			.bind("mouseenter.panel", function() {
 				if (self.options.disabled) {
 					return;
 				}
 				$(this).addClass('ui-state-hover');
 			})
-			.bind("mouseleave.panel", function() { $(this).removeClass('ui-state-hover'); })
-			.bind("focus.panel", function() { $(this).addClass('ui-state-focus'); })
-			.bind("blur.panel", function() { $(this).removeClass('ui-state-focus'); })
+			.bind("mouseleave.panel", function() {
+				$(this).removeClass('ui-state-hover');
+			})
+			.bind("focus.panel", function() {
+				$(this).addClass('ui-state-focus');
+			})
+			.bind("blur.panel", function() {
+				$(this).removeClass('ui-state-focus');
+			})
 			.bind("click.panel", function(e) {
 				if (self.options.disabled) {
 					return;
